fb1b4de924bd1890a16ce48189f61ac747b32897
rolanusonov <rolanusonov031209@gmail.com>
2022-04-24 20:47:04 +0600

    itni

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
     <div className="App">
 
         <h1>aken=mir</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores, aut commodi cum deleniti deserunt eos error est facilis, ipsa nostrum numquam perspiciatis quisquam sint sit ullam ut vitae voluptas voluptatum! Aut consequuntur doloribus eaque eius eos error, et eum explicabo inventore iste labore maxime molestiae necessitatibus neque, porro praesentium quasi similique sit sunt voluptates. Blanditiis est illo inventore libero molestias natus nostrum quis reiciendis sequi. Animi delectus distinctio eaque eligendi est id itaque, libero maxime nihil perferendis placeat possimus quia recusandae reiciendis, sit voluptatem voluptatum. Dolores, eius id iure iusto libero molestias nisi, qui, repudiandae similique ullam voluptas!
+        </p>
      </div>
   )
 }
